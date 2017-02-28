c613ac3248fd6feeb61dda1b511006b482c14d91
awhite9 <ajumper12@gmail.com>
2017-02-28 13:06:34 -0600

    email and SMS working

---
+++ app/controllers/SMSAmazon.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.MessageAttributeValue;
+import com.amazonaws.services.sns.model.PublishRequest;
+import com.amazonaws.services.sns.model.PublishResult;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SMSAmazon
+{
+    public static void main(String[] args) {
+        AmazonSNSClient snsClient = new AmazonSNSClient();
+        String message = "Are you on your way?";
+        String phoneNumber = "+15017225";
+        Map<String, MessageAttributeValue> smsAttributes =
+                new HashMap<String, MessageAttributeValue>();
+        smsAttributes.put("AWS.SNS.SMS.SenderID", new MessageAttributeValue()
+                .withStringValue("mySenderID") //The sender ID shown on the device.
+                .withDataType("String"));
+        smsAttributes.put("AWS.SNS.SMS.MaxPrice", new MessageAttributeValue()
+                .withStringValue("0.50") //Sets the max price to 0.50 USD.
+                .withDataType("Number"));
+        smsAttributes.put("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
+                .withStringValue("Promotional") //Sets the type to promotional.
+                .withDataType("String"));
+        sendSMSMessage(snsClient, message, phoneNumber, smsAttributes);
+    }
+
+    public static void sendSMSMessage(AmazonSNSClient snsClient, String message,
+                                      String phoneNumber, Map<String, MessageAttributeValue> smsAttributes) {
+        PublishResult result = snsClient.publish(new PublishRequest()
+                .withMessage(message)
+                .withPhoneNumber(phoneNumber)
+                .withMessageAttributes(smsAttributes));
+        System.out.println(result); // Prints the message ID.
+    }
+}
